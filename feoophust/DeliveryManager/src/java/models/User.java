6490487abc5a7236ed48146de6cf231bff3513c3
Đình Lộc <163549934+Roku2004@users.noreply.github.com>
2024-12-07 17:45:22 +0700

    Add files via upload

---
+++ feoophust/DeliveryManager/src/java/models/User.java
@@ -0,0 +1,62 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package models;
+
+/**
+ *
+ * @author Admin
+ */
+public class User {
+
+    private int user_id;
+    private String full_name;
+    private String phone;
+    private Account account_id;
+
+    public User() {
+    }
+
+    public User(int user_id, String full_name, String phone, Account account_id) {
+        this.user_id = user_id;
+        this.full_name = full_name;
+        this.phone = phone;
+        this.account_id = account_id;
+    }
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getFull_name() {
+        return full_name;
+    }
+
+    public void setFull_name(String full_name) {
+        this.full_name = full_name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public Account getAccount_id() {
+        return account_id;
+    }
+
+    public void setAccount_id(Account account_id) {
+        this.account_id = account_id;
+    }
+    
+    
+    
+}
