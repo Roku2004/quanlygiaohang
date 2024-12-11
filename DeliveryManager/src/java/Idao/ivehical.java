31932a7374b03eda8b03bf811cca939a858a323e
Đình Lộc <163549934+Roku2004@users.noreply.github.com>
2024-12-11 21:23:17 +0700

    Add files via upload

---
+++ DeliveryManager/src/java/Idao/ivehical.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package Idao;
+
+import java.util.ArrayList;
+import models.Vehical;
+
+/**
+ *
+ * @author ASUS
+ */
+public interface ivehical {
+    public void addVehical(String vehicalType,double capacity,String numberPlate);
+    
+    public ArrayList<Vehical> getVehical();
+}
---
+++ DeliveryManager/src/java/controllers/Admin/UpdateOrderAdminservlet.java
@@ -0,0 +1,102 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+
+package controllers.Admin;
+
+import dao.orderdao;
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author ASUS
+ */
+public class UpdateOrderAdminservlet extends HttpServlet {
+   
+    /** 
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet UpdateOrderAdminservlet</title>");  
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet UpdateOrderAdminservlet at " + request.getContextPath () + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    } 
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /** 
+     * Handles the HTTP <code>GET</code> method.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        processRequest(request, response);
+    } 
+
+    /** 
+     * Handles the HTTP <code>POST</code> method.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        int order_id = Integer.parseInt(request.getParameter("order_id"));
+        String Sender = request.getParameter("sender");
+        String Receiver = request.getParameter("receiver");
+        String Origin_address = request.getParameter("origin_address");
+        String Destination_address = request.getParameter("destination_address");
+        String Sender_phone = request.getParameter("sender_phone");
+        String Receiver_phone = request.getParameter("receiver_phone");
+        String Status = request.getParameter("status");
+        int Weight = Integer.parseInt(request.getParameter("weight"));
+        double Total_Cost = Double.parseDouble(request.getParameter("total"));
+        String choice = request.getParameter("Payment");
+        boolean Payment_status;
+        if(choice.equals("true")){
+            Payment_status = true;
+        }
+        else{
+            Payment_status = false;
+        }
+        orderdao od = new orderdao();
+        od.UpdateOrderAdmin(order_id, Sender, Receiver, Origin_address, Destination_address, Sender_phone, Receiver_phone, Weight, Status, Weight, true);
+        response.sendRedirect("OrderListAdmin.jsp");
+    }
+
+    /** 
+     * Returns a short description of the servlet.
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
